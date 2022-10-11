b3a13c391f41c86a9e4108650e388a2df823f350
Claudiney <claudineygui@gmail.com>
2022-10-11 11:25:43 -0300

    Aula05

---
+++ Aula05/aula05.cs
@@ -0,0 +1,16 @@
+using System;
+
+class aula05
+{
+    static void Main(){
+        int v1, v2, soma;
+        string nome;
+
+        Console.Write("Digite o primeiro valor: ");
+        v1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor: ");
+        v2 = int.Parse(Console.ReadLine());
+        soma = v1 + v2;
+        Console.WriteLine("A soma de {0} com {1} é igual a {2}", v1, v2, soma);
+    }
+}
