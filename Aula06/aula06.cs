22644e666c81f804f4b7c4b82496ec0ecfd73112
Claudiney <claudineygui@gmail.com>
2022-10-11 12:02:53 -0300

    Aula06

---
+++ Aula06/aula06.cs
@@ -0,0 +1,13 @@
+using System;
+
+class aula06{
+    enum DiasSemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado};
+    
+    static void Main(){
+        // DiasSemana ds = DiasSemana.Domingo;
+        DiasSemana qua = (DiasSemana)3;
+
+
+        Console.WriteLine(qua);
+    }
+}
