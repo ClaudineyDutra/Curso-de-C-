1a2510cb162ff52b2a72cdad37329b805acca91a
Claudiney <claudineygui@gmail.com>
2022-10-11 12:10:49 -0300

    Aula07

---
+++ Aula07/aula07.cs
@@ -0,0 +1,13 @@
+using System;
+
+class aula07{
+    
+    static void Main(){
+        // Type Cast
+
+        float num1 = 10.5f;
+        int num2 = (int)num1; //Conversão Explícita de TypeCast
+
+        Console.WriteLine(num2);
+    }
+}
